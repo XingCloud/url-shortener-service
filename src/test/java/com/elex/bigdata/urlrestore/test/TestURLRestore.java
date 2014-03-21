d5fadb09eeb74e40c3a0b8fa139480fe0c3c6e09
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-21 11:57:12 +0800

    Add setenv.sh

--- src/test/java/com/elex/bigdata/urlrestore/test/TestURLRestore.java
+++ src/test/java/com/elex/bigdata/urlrestore/test/TestURLRestore.java
@@ -2,10 +2,16 @@ package com.elex.bigdata.urlrestore.test;
 
 import com.elex.bigdata.ro.RedisOperationException;
 import com.elex.bigdata.urlrestore.exception.URLRestoreException;
-import com.elex.bigdata.urlrestore.util.URLRestoreCacheOperation;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -15,7 +21,32 @@ public class TestURLRestore {
 
   @Test
   public void test() throws RedisOperationException, IOException, URLRestoreException, URISyntaxException {
-    URLRestoreCacheOperation operation = URLRestoreCacheOperation.getInstance();
-    System.out.println(operation.restoreURL("goo.mx/raYNBj"));
+    URIBuilder builder = new URIBuilder();
+    builder.setScheme("http");
+    builder.setHost("xa.xingcloud.com");
+    builder.setPort(80);
+    builder.setPath("/v4/sof-isafe/wuzijing");
+    builder.addParameter("action", "qf.svc.show.5.1.11");
+    URI uri = builder.build();
+    HttpGet httpGet = new HttpGet(uri);
+    System.out.println(uri);
+    HttpClient client = HttpClients.createDefault();
+    int ok = 0, failed = 0;
+    HttpResponse response;
+    StatusLine statusLine;
+    int cnt = 0;
+    for (int i = 0; i < 1000; i++) {
+      response = client.execute(httpGet);
+      statusLine = response.getStatusLine();
+      if (200 == statusLine.getStatusCode()) {
+        ++ok;
+      } else {
+        ++failed;
+      }
+      ++cnt;
+      System.out.println("cnt=" + cnt);
+    }
+
+    System.out.println("OK=" + ok + ", Failed=" + failed);
   }
 }
